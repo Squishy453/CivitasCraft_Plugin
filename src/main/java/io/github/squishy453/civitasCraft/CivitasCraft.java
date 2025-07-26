f6841c0d65d04b872ac4d6591c2323063498ffcc
Squishy453 <cbushor1204@gmail.com>
2025-07-25 20:40:55 -0400

    git config --global user.email "cbushor1204@gmail.com"

---
+++ src/main/java/io/github/squishy453/civitasCraft/CivitasCraft.java
@@ -0,0 +1,26 @@
+package io.github.squishy453.civitasCraft;
+
+import io.github.squishy453.civitasCraft.commands.civitasCommand;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CivitasCraft extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("CivitasCraft has been enabled.");
+
+        this.getCommand("civitas").setExecutor(new CivitasCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("CivitasCraft has been disabled.");
+    }
+
+
+}
---
+++ src/main/java/io/github/squishy453/civitasCraft/commands/civitasCommand.java
@@ -0,0 +1,10 @@
+package io.github.squishy453.civitasCraft.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class civitasCommand()
